6c7e1da3490a040254a3db5e1f992a309b8b372b
jiwei <jiwei@qq.com>
2020-09-29 18:33:10 +0800

    this version add a class Persion

--- Hello.java
+++ Hello.java
@@ -4,3 +4,26 @@ public class Hello{
 		System.out.println("Second modify");
 	}
 }
+class Person{
+private String name;
+private int age;
+private char gender;
+
+public Person(){
+}
+public Person(String name, int age, char gender){
+	this.name=name;
+	this.age=age;
+	this.gender=gender;
+}	
+public String getName(){
+	return this.name;
+}
+public void setName(String name){
+	this.name=name;
+}
+
+
+
+}
+
