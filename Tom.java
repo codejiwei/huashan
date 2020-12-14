263da5e4bf8c698c99d6508499dbbae153202bd2
codejiwei <1622264511@qq.com>
2020-12-14 20:42:33 +0800

    Tom

---
+++ Tom.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName Tom
+ * @Description TODO
+ * @Author codejiwei
+ * @Date 2020/12/14 20:41
+ * @Version 1.0
+ **/
+public class Tom {
+    public static void main(String[] args) {
+
+        System.out.println("Tom Tom");
+
+
+    }
+}
